3088109eadab4ce9a09a3844ec1b4cdb2355f468
phuonganhniie <anhdp@hanet.com>
2023-05-09 16:15:55 +0700

    chore: remove uneccessary files and comments

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+def calculate_peak_daily_demand(daily_usage):
+    """
+    Calculate the peak daily demand for cars and motorbikes.
+
+    :param daily_usage: pd.DataFrame, the daily usage data
+    :return: dict, the peak daily demand for cars and motorbikes
+    """
+    car_peak = np.max(daily_usage['Cars'])
+    motorbikes_peak = np.max(daily_usage['Motorbikes'])
+    return {"Cars": car_peak, "Motorbikes": motorbikes_peak}
+
 def calculate_average_daily_demand(daily_usage):
     """
     Calculate the average daily demand for cars and motorbikes.
@@ -10,16 +21,3 @@ def calculate_average_daily_demand(daily_usage):
     cars_average = np.mean(daily_usage['Cars'])
     motorbikes_average = np.mean(daily_usage['Motorbikes'])
     return {"Cars": cars_average, "Motorbikes": motorbikes_average}
-
-def calculate_peak_daily_demand(daily_usage):
-    """
-    Calculate the peak daily demand for cars and motorbikes.
-
-    :param daily_usage: pd.DataFrame, the daily usage data
-    :return: dict, the peak daily demand for cars and motorbikes
-    """
-    car_peak = np.max(daily_usage['Cars'])
-    motorbikes_peak = np.max(daily_usage['Motorbikes'])
-    print("cars peak: ", car_peak)
-    print("moto peak: ", motorbikes_peak)
-    return {"Cars": car_peak, "Motorbikes": motorbikes_peak}
