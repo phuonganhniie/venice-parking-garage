fb1aead65941a214ae1ab2e0d6d063a589474b28
phuonganhniie <anhdp@hanet.com>
2023-05-09 17:59:09 +0700

    feat: add basic html template for showing clearly result

---
+++ main.py
@@ -0,0 +1,70 @@
+# import argparse
+# import data_processing as dp
+# import analysis as an
+# import recommendations as rec
+
+# def main(args):
+#     # Read and process data
+#     data = dp.read_data(args.input_file)
+#     daily_usage = dp.calculate_daily_usage(data)
+    
+#     # Analyze data
+#     average_daily_demand = an.calculate_average_daily_demand(daily_usage)
+#     peak_daily_demand = an.calculate_peak_daily_demand(daily_usage)
+    
+#     # Calculate recommendations
+#     slots = rec.calculate_slots(average_daily_demand, peak_daily_demand, args.target_occupancy_rate)
+#     garage_dimensions = rec.recommend_garage_dimensions(slots, args.levels)
+    
+#     # Print results
+#     print("Garage dimensions:", garage_dimensions)
+    
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description='Garage Dimension Recommender')
+#     parser.add_argument('--input_file', type=str, default="data/historical_data.csv", help='Path to the input CSV file')
+#     parser.add_argument('--target_occupancy_rate', type=float, default=0.8, help='Target occupancy rate (0-1)')
+#     parser.add_argument('--levels', type=int, default=1, help='Number of parking levels')
+#     args = parser.parse_args()
+    
+#     main(args)
+
+import src.data_processing as dp
+import src.analysis as an
+import src.recommendations as rec
+from flask import Flask, render_template, request
+
+CSV_FILE_PATH = "data/historical_data.csv"
+
+app = Flask(__name__)
+
+def calculate_garage_dimensions(occupancy_rate, parking_levels, csv_file_path):
+    # Read and process data
+    data = dp.read_data(csv_file_path)
+    daily_usage = dp.calculate_daily_usage(data)
+
+    # Analyze data
+    average_daily_demand = an.calculate_average_daily_demand(daily_usage)
+    peak_daily_demand = an.calculate_peak_daily_demand(daily_usage)
+
+    # Calculate recommendations
+    slots = rec.calculate_slots(average_daily_demand, peak_daily_demand, occupancy_rate)
+    garage_dimensions = rec.recommend_garage_dimensions(slots, parking_levels)
+
+    return garage_dimensions
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            occupancy_rate = float(request.form['occupancy_rate'])
+            parking_levels = int(request.form['levels'])
+        except ValueError:
+            return "Invalid input values. Please provide valid numbers."
+
+        garage_dimensions = calculate_garage_dimensions(occupancy_rate, parking_levels, CSV_FILE_PATH)
+        return render_template('index.html', dimensions=garage_dimensions, occupancy_rate=occupancy_rate, levels=parking_levels)
+    else:
+        return render_template('index.html', occupancy_rate=0.8, levels=1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
